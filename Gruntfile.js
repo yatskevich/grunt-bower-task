46c60573a05b1cc4eea86afd9ef513da504d6407
Ivan Yatskevich <ivan@yatskevich.com>
2012-11-26 14:49:30 +0300

    fix testing setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,8 +45,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
 
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['test']);
 
 };
