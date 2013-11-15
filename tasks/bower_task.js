0dcfe5ca5f8eed3bbc0883cab7e0363ec86d78ab
KyorCode <t-c@live.be>
2013-11-15 15:26:19 +0100

    Fixed JSHint errors
    Fixed JSHint errors

--- tasks/bower_task.js
+++ tasks/bower_task.js
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
         cleanTargetDir: false,
         cleanBowerDir: false,
         targetDir: './lib',
-		overrideBowerDirectory : false,
+				overrideBowerDirectory : false,
         layout: 'byType',
         install: true,
         verbose: false,
@@ -74,14 +74,14 @@ module.exports = function(grunt) {
           });
         });
       };
-	  
-	if(options.overrideBoweDirectory){
-		bower.config.directory = options.targetDir;
-	}
+    
+    if(options.overrideBoweDirectory){
+      bower.config.directory = options.targetDir;
+    }
 	
     options.cwd = bower.config.cwd = path.resolve(options.base || options.cwd || process.cwd());
-    var bowerDir = path.resolve(options.cwd + '/' + bower.config.directory),
-      targetDir = options.targetDir = path.resolve(options.cwd + '/' + options.targetDir);
+    var bowerDir = path.resolve(options.cwd + '/' + bower.config.directory);
+    var targetDir = options.targetDir = path.resolve(options.cwd + '/' + options.targetDir);
 
     log.logger = options.verbose ? grunt.log : grunt.verbose;
     options.layout = LayoutsManager.getLayout(options.layout, fail);
