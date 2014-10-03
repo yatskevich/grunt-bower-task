ba5c7b25372ddc65f557618ea496a9a1872bbd0d
tporadowski <tomasz@poradowski.net>
2014-10-03 16:22:47 +0200

    Use exportsOverride to rename target folders
    - exportOverride can now use an extended definition that has
    "packageName" naming convention which is run through Handlebars with
    package's metadata (so one can install to i.e. "js/jquery-2.1.1" based
    on metadata of Bower packages)
    - added dependencies to Q and Handlebars

--- tasks/lib/bower_assets.js
+++ tasks/lib/bower_assets.js
@@ -3,6 +3,8 @@ var Emitter = require('events').EventEmitter;
 var path = require('path');
 var grunt = require('grunt');
 var packageMatcher = require('./package_matcher');
+var Q = require('q');
+var Handlebars = require('handlebars');
 
 var Assets = function(cwd, componentsDir) {
   this._assets = {};
@@ -10,15 +12,24 @@ var Assets = function(cwd, componentsDir) {
   this._componentsDir = componentsDir;
 };
 
-Assets.prototype.addOverridden = function(override, pkg) {
+Assets.prototype.addOverridden = function(override, pkg, pkgMeta, exportsOverride) {
   var pkgPath = path.join(this._componentsDir, pkg);
 
-  _(override).each(function(overriddenPaths, assetType) {
+  //take "packageName" specified for given override (or for master "exportsOverride")
+  //  and run it through Handlebars to further customize name of the package (and thus target folder)
+  var tmpl = Handlebars.compile(getPackageName(override) || getPackageName(exportsOverride) || pkg);
+  //provide context with:
+  //  meta - package metadata (as returned by Bower)
+  //  version - object with "major", "minor" and "revision" properties (extracted from metadata's version)
+  //  version_safe - metatdata's version with all non-dot and non-digit characters replaced with "_"
+  pkg = tmpl({ meta: pkgMeta, version: getPackageVersion(pkgMeta), version_safe: pkgMeta.version.replace(/[^0-9.]/, '_') });
+
+  _(getOverrides(override)).each(function(overriddenPaths, assetType) {
     this.addAssets(overriddenPaths, pkg, assetType, pkgPath);
   }, this);
 };
 
-Assets.prototype.addUntyped = function(pkgFiles, pkg) {
+Assets.prototype.addUntyped = function(pkgFiles, pkg, pkgMeta) {
   this.addAssets(pkgFiles, pkg, '__untyped__');
 };
 
@@ -41,6 +52,39 @@ Assets.prototype.toObject = function() {
   return _.clone(this._assets);
 };
 
+/**
+ * Checks if parameter contains an extended definition that has "packageName"
+ * and "overrides" properties.
+ */
+function isExtendedOverride(overrides) {
+  return overrides && _(overrides).has('packageName') && _(overrides).has('overrides');
+}
+
+/**
+ * Gets overrides from an extended definition or returns "overrides" as is.
+ */
+function getOverrides(overrides) {
+  return isExtendedOverride(overrides) && _.isObject(overrides.overrides) ? overrides.overrides : overrides;
+}
+
+/**
+ * Gets "packageName" property if given an extended overrides definition or returns null.
+ */
+function getPackageName(overrides) {
+  return isExtendedOverride(overrides) ? overrides.packageName : null;
+}
+
+/**
+ * Gets Bower package version from its metadata.
+ *
+ * @param pkgMeta package metadata (returned by "bower.commands.info")
+ * @returns object with "major", "minor" and "revision" properties (extracted from metadata's version)
+ */
+function getPackageVersion(pkgMeta) {
+  var parts = pkgMeta.version.split(/[^0-9]/).concat([0, 0, 0]);
+  return { major: parts[0], minor: parts[1], revision: parts[2] };
+}
+
 
 var BowerAssets = function(bower, cwd) {
   this.bower = bower;
@@ -55,11 +99,13 @@ BowerAssets.prototype.constructor = BowerAssets;
 BowerAssets.prototype.get = function() {
   var bower = this.bower;
   var bowerConfig = grunt.file.readJSON(path.join(this.cwd, this.config));
-  var exportsOverride = bowerConfig.exportsOverride;
+  var exportsOverride = bowerConfig.exportsOverride || {};
 
   var paths = bower.commands.list({paths: true});
   paths.on('end', function(data) {
-    this.emit('end', this.mergePaths(data, exportsOverride ? exportsOverride : {}));
+    this.mergePaths(data, exportsOverride, bower, function(retData) {
+      this.emit('end', retData);
+    }.bind(this));
   }.bind(this));
   paths.on('error', function(err) {
     this.emit('error', err);
@@ -72,27 +118,42 @@ BowerAssets.prototype.get = function() {
  *
  * @param bowerComponents - output of 'bower list' command
  * @param overrides - overrides coming from 'bower.json'
+ * @param bower - reference to Bower
+ * @param callback - function to be called back with 1 argument (object with assets)
  *
  * @returns assets grouped by component and type
  */
-BowerAssets.prototype.mergePaths = function(bowerComponents, overrides) {
+BowerAssets.prototype.mergePaths = function(bowerComponents, overrides, bower, callback) {
   var findOverride = function(pkg) {
-    return _(overrides).find(function(override, override_key) {
+    return _(getOverrides(overrides)).find(function(override, override_key) {
       return packageMatcher.matches(pkg, override_key);
     });
   };
 
-  _(bowerComponents).each(function(pkgFiles, pkg) {
-    var activeOverride = findOverride(pkg);
+  var that = this;
+  Q.allSettled(
+    _(bowerComponents).map(function(pkgFiles, pkg) {
+      var deferred = Q.defer();
 
-    if (activeOverride) {
-      this.assets.addOverridden(activeOverride, pkg);
-    } else {
-      this.assets.addUntyped(pkgFiles, pkg);
-    }
-  }, this);
+      //get package metadata and pass it to assets' aggregation functions
+      bower.commands.info(pkg).on('end', function(pkginfo) {
+        var activeOverride = findOverride(pkg);
+        pkginfo = pkginfo.latest || pkginfo;
 
-  return this.assets.toObject();
+        if (activeOverride) {
+          that.assets.addOverridden(activeOverride, pkg, pkginfo, overrides);
+        } else {
+          that.assets.addUntyped(pkgFiles, pkg, pkginfo);
+        }
+
+        deferred.resolve(true);
+      });
+
+      return deferred.promise;
+    })
+  ).then(function() {
+    callback(that.assets.toObject());
+  });
 };
 
 module.exports = BowerAssets;
