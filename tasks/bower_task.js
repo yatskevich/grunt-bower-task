f95e4c5b566d800865a21b8acd213d6b2666c1a3
Hobart <marco.klein@avsn.de>
2016-04-28 18:23:42 +0200

    Making bowerOptions work
    In the newest version of bower, bower options arent considered because the call order of bower.commands.install changed. I corrected this, now it works.

--- tasks/bower_task.js
+++ tasks/bower_task.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
   }
 
   function install(options, callback) {
-    bower.commands.install([], options.bowerOptions)
+    bower.commands.install([], undefined, options.bowerOptions)
       .on('log', function(result) {
         log(['bower', result.id.cyan, result.message].join(' '));
       })
